c2e9fbab83064ccfa5a87c31c2515e482713f4ee
Luca Tricarico <luc.tricarico@gmail.com>
2026-03-03 23:00:48 +0100

    Fix view books e layer geoserver

--- backend/LibraryService/src/main/resources/db/migration/V1.0.4__create_view_books.sql
+++ backend/LibraryService/src/main/resources/db/migration/V1.0.4__create_view_books.sql
@@ -1,8 +1,8 @@
-create view books_map_v(book_id, title, author, location, thumbnail_url) as
+create or replace view books_map_v(book_id, title, author, location, thumbnail_url) as
 SELECT b.id         AS book_id,
        b.title,
        b.author,
-       b.location,
+       b.location::geometry(Point, 4326) AS location,
        im.url_image AS thumbnail_url
 FROM books b
          LEFT JOIN books_images im ON b.id = im.book_id
